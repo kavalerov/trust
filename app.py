b3499e3d89bd0b6c1e7ef3733b7f9bd35f2a372e
Ivan Kavalerov <ivan@kavalerov.net>
2023-02-08 20:49:37 +0100

    add remaining features

--- app.py
+++ app.py
@@ -8,13 +8,17 @@ from plotly import express as px
 
 def process(history, num_people, we_dict):
     print(history)
-    regex = r"^([1-9]|([012][0-9])|(3[01]))\/([0]{0,1}[1-9]|1[012])\/(\d\d\d\d),\s([0-1]?[0-9]|2?[0-3]):([0-5]\d) - ([a-zA-Z ]*):"
+    regex = r"^([1-9]|([012][0-9])|(3[01]))\/([0]{0,1}[1-9]|1[012])\/(\d\d\d\d),\s([0-1]?[0-9]|2?[0-3]):([0-5]\d) - ([a-zA-Z ]*):([^\n\r]*)"
     intervention_datetimes = re.findall(regex, history, re.MULTILINE)
     dates = []
     active_participants = []
+    analysis_words = we_dict.split("\n")
     df = pd.DataFrame(columns=["total"])
-    print(intervention_datetimes)
+    analysis_words.append("total")
+    print(analysis_words)
+    words_df = pd.DataFrame(columns=analysis_words)
     for item in intervention_datetimes:
+        text = item[8]
         participant_name = item[7]
         if participant_name not in active_participants:
             active_participants.append(participant_name)
@@ -26,9 +30,15 @@ def process(history, num_people, we_dict):
         full_date = day + "/" + item[3] + "/" + item[4]
         if full_date not in dates:
             df.loc[full_date] = [0 for i in range(len(active_participants) + 1)]
+            if len(analysis_words) > 0:
+                words_df.loc[full_date] = [0 for i in range(len(analysis_words))]
             dates.append(full_date)
         df[participant_name].loc[[full_date]] += 1
         df["total"].loc[full_date] += 1
+        for word in analysis_words:
+            if word in text:
+                words_df[word].loc[full_date] += 1
+                words_df["total"].loc[full_date] += 1
 
     # formatted_dates = []
     # for key, value in dates.items():
@@ -38,18 +48,22 @@ def process(history, num_people, we_dict):
     num_of_questions = history.count("?")
     bar_plot = px.bar(df, x=df.index, y="total")
     stacked_bar_plot = px.bar(df, x=df.index, y=active_participants, barmode="stack")
-
+    word_bar_plot = px.bar(df, x=words_df.index, y="total")
     proportion = "{:2.{decimal}f}%".format(
         (len(active_participants) / int(num_people) if num_people != "" else 0) * 100,
         decimal=1,
     )
-
+    # df["Date"] = df.index
+    df.insert(0, "Date", df.index)
+    words_df.insert(0, "Date", words_df.index)
+    # words_df["Date"] = words_df.index
     return (
         df,
         bar_plot,
         stacked_bar_plot,
         proportion,
-        "",
+        words_df,
+        word_bar_plot,
         str(num_of_questions),
     )
 
@@ -90,20 +104,12 @@ with gr.Blocks() as demo:
     output_interventions_plot_stacked = gr.Plot()
     output_proportion = gr.Textbox(
         "Proportion of 'we' words will be displayed here",
-        max_lines=500,
-        lines=10,
         label="Proportion of active usage",
     )
-    output_we_words = gr.Textbox(
-        "Usage of 'we' words will be displayed here",
-        max_lines=500,
-        lines=10,
-        label="Usage of preset words",
-    )
+    output_we_words = gr.DataFrame(headers=["Date", "Number of words usage"])
+    output_we_words_plot = gr.Plot()
     output_num_of_question_marks = gr.Textbox(
         "Question marks usage will be displayed here",
-        max_lines=500,
-        lines=10,
         label="Number of question marks",
     )
     submit_btn.click(
@@ -115,6 +121,7 @@ with gr.Blocks() as demo:
             output_interventions_plot_stacked,
             output_proportion,
             output_we_words,
+            output_we_words_plot,
             output_num_of_question_marks,
         ],
     )
