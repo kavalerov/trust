7a7a362c8d7e2dc46f881249bd82cfe42abb1d7c
Ivan Kavalerov <ivan@kavalerov.net>
2023-02-17 08:47:45 +0000

    Fix regex for names

--- app.py
+++ app.py
@@ -47,8 +47,10 @@ def draw_histogram(text):
 
 def process(history, num_people, we_dict):
     print(history)
-    regex = r"^([1-9]|([012][0-9])|(3[01]))\/([0]{0,1}[1-9]|1[012])\/(\d\d\d\d),\s([0-1]?[0-9]|2?[0-3]):([0-5]\d) - ([a-zA-Z ]*):([^\n\r]*)"
+    regex = r"^([1-9]|([012][0-9])|(3[01]))\/([0]{0,1}[1-9]|1[012])\/(\d\d\d\d),\s([0-1]?[0-9]|2?[0-3]):([0-5]\d) - ([a-zA-Z ()-]*):([^\n\r]*)"
     intervention_datetimes = re.findall(regex, history, re.MULTILINE)
+    if len(intervention_datetimes) == 0:
+        regex = 
     dates = []
     active_participants = []
     analysis_words = we_dict.split("\n")
