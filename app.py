cf0c8c775c0c565df25230f0aa7cb7e2eb7e7cc9
Ivan Kavalerov <ivan@kavalerov.net>
2023-02-17 09:29:04 +0000

    Fix old regex

--- app.py
+++ app.py
@@ -46,12 +46,12 @@ def draw_histogram(text):
 
 
 def process(history, num_people, we_dict):
-    regex = r"^([1-9]|([012][0-9])|(3[01]))\/([0]{0,1}[1-9]|1[012])\/(\d\d\d\d),\s([0-1]?[0-9]|2?[0-3]):([0-5]\d) - ([a-zA-Z ()-]*):((.|\n)+?(?=(\[|\Z)))"
+    regex = r"^([1-9]|([012][0-9])|(3[01]))\/([0]{0,1}[1-9]|1[012])\/(\d\d\d\d),\s([0-1]?[0-9]|2?[0-3]):([0-5]\d) - ([a-zA-Z ()-+0-9]*):((.|\n)+?(?=((^([1-9]|([012][0-9])|(3[01]))\/([0]{0,1}[1-9]|1[012])\/(\d\d\d\d),\s([0-1]?[0-9]|2?[0-3]):([0-5]\d))|\Z)))"
     intervention_datetimes = re.findall(regex, history, re.MULTILINE)
     format = 1
     print("First match: " + str(len(intervention_datetimes)))
     if len(intervention_datetimes) == 0:
-        regex = r"^\[([0-1]?[0-9]|2?[0-3]):([0-5]\d),\s([1-9]|([012][0-9])|(3[01]))\/([0]{0,1}[1-9]|1[012])\/(\d\d\d\d)\]\s([a-zA-Z ()-]*):((.|\n)+?(?=(\[|\Z)))"
+        regex = r"^\[([0-1]?[0-9]|2?[0-3]):([0-5]\d),\s([1-9]|([012][0-9])|(3[01]))\/([0]{0,1}[1-9]|1[012])\/(\d\d\d\d)\]\s([a-zA-Z ()-+0-9]*):((.|\n)+?(?=(\[|\Z)))"
         intervention_datetimes = re.findall(regex, history, re.MULTILINE)
         format = 2
         print("Second match: " + str(len(intervention_datetimes)))
