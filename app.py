d8ad60cccbd8e0ba254ffa0003e1006712ec3f06
Ivan Kavalerov <ivan@kavalerov.net>
2023-03-17 08:26:31 +0000

    Add saving lists to Supabase

--- app.py
+++ app.py
@@ -1,9 +1,55 @@
+import os
 import re
+from typing import List
 
 import gradio as gr
 import numpy as np
 import pandas as pd
+from dotenv import load_dotenv
 from plotly import express as px
+from supabase import Client, create_client
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+
+def get_dictionaries_titles() -> List[str]:
+    # get the dictionaries
+    dictionaries = supabase.from_("dictionaries").select("title").execute()
+    print("Getting new titles")
+    return [d["title"] for d in dictionaries.data]
+
+
+def show_dictionary(title: str) -> str:
+    # get the dictionary
+    dictionary = (
+        supabase.from_("dictionaries").select("words").eq("title", title).execute()
+    )
+    return dictionary.data[0]["words"]
+
+
+def save_dictionary(title: str, words: str) -> str:
+    # get the dictionary
+    dictionary = (
+        supabase.from_("dictionaries").select("words").eq("title", title).execute()
+    )
+    if len(dictionary.data) == 0:
+        dictionary = (
+            supabase.from_("dictionaries")
+            .insert({"title": title, "words": words})
+            .execute()
+        )
+    else:
+        dictionary = (
+            supabase.from_("dictionaries")
+            .update({"words": words})
+            .eq("title", title)
+            .execute()
+        )
+    return dictionary.data[0]["words"]
 
 
 # function that takes in a string and returns a list of words
@@ -130,63 +176,132 @@ def process(history, num_people, we_dict):
 
 with gr.Blocks() as demo:
     gr.Label("Chat history analysis")
-    chat_history = gr.Textbox(
-        "",
-        interactive=True,
-        max_lines=500,
-        lines=10,
-        label="Chat history",
-    )
-    num_of_people = gr.Textbox(
-        "",
-        interactive=True,
-        max_lines=1,
-        lines=1,
-        label="Total number of people in the chat",
-    )
-    we_dictionary = gr.Textbox(
-        "",
-        interactive=True,
-        max_lines=500,
-        lines=10,
-        label="Dictionary of words to check for occurance",
-    )
-    submit_btn = gr.Button(value="Submit")
-    gr.Label("Output")
-    # output_interventions = gr.Textbox(
-    #     "Interventions will be displayed here",
-    #     max_lines=500,
-    #     lines=10,
-    #     label="Interventions",
-    # )
-    output_interventions = gr.DataFrame(headers=["Date", "Number of interventions"])
-    output_interventions_plot = gr.Plot()
-    output_interventions_plot_stacked = gr.Plot()
-    output_proportion = gr.Textbox(
-        "Proportion of 'we' words will be displayed here",
-        label="Proportion of active usage",
-    )
-    output_we_words = gr.DataFrame(headers=["Date", "Number of words usage"])
-    output_we_words_plot = gr.Plot()
-    output_num_of_question_marks = gr.Textbox(
-        "Question marks usage will be displayed here",
-        label="Number of question marks",
-    )
-    submit_btn.click(
-        process,
-        inputs=[chat_history, num_of_people, we_dictionary],
-        outputs=[
-            output_interventions,
-            output_interventions_plot,
-            output_interventions_plot_stacked,
-            output_proportion,
-            output_we_words,
-            output_we_words_plot,
-            output_num_of_question_marks,
-        ],
-    )
+    dict_list = get_dictionaries_titles()
+    print(dict_list)
+    with gr.Tab("Analysis") as analysis_tab:
+        dict_reload = gr.Button("Reload dictionaries")
+        chat_history = gr.Textbox(
+            "",
+            interactive=True,
+            max_lines=500,
+            lines=10,
+            label="Chat history",
+        )
+        num_of_people = gr.Textbox(
+            "",
+            interactive=True,
+            max_lines=1,
+            lines=1,
+            label="Total number of people in the chat",
+        )
+        dict_dropdown = gr.Dropdown(
+            dict_list, label="Dictionary of words to check for occurance"
+        )
+        we_dictionary = gr.Textbox(
+            "",
+            interactive=False,
+            max_lines=500,
+            lines=10,
+            label="Full dictionary. Please go to the 'Dictionaries' tab to edit a dictionary, or 'Add Dictionaries' to add a new one.",
+        )
+        dict_reload.click(get_dictionaries_titles, inputs=[], outputs=[dict_dropdown])
 
-    gr.Label("Output")
+        dict_dropdown.change(
+            show_dictionary,
+            inputs=[dict_dropdown],
+            outputs=[we_dictionary],
+        )
+
+        submit_btn = gr.Button(value="Submit")
+        gr.Label("Output")
+        # output_interventions = gr.Textbox(
+        #     "Interventions will be displayed here",
+        #     max_lines=500,
+        #     lines=10,
+        #     label="Interventions",
+        # )
+        output_interventions = gr.DataFrame(headers=["Date", "Number of interventions"])
+        output_interventions_plot = gr.Plot()
+        output_interventions_plot_stacked = gr.Plot()
+        output_proportion = gr.Textbox(
+            "Proportion of 'we' words will be displayed here",
+            label="Proportion of active usage",
+        )
+        output_we_words = gr.DataFrame(headers=["Date", "Number of words usage"])
+        output_we_words_plot = gr.Plot()
+        output_num_of_question_marks = gr.Textbox(
+            "Question marks usage will be displayed here",
+            label="Number of question marks",
+        )
+        submit_btn.click(
+            process,
+            inputs=[chat_history, num_of_people, we_dictionary],
+            outputs=[
+                output_interventions,
+                output_interventions_plot,
+                output_interventions_plot_stacked,
+                output_proportion,
+                output_we_words,
+                output_we_words_plot,
+                output_num_of_question_marks,
+            ],
+        )
+    with gr.Tab("Dictionaries") as dictionaries_tab:
+        gr.Label(
+            "Choose dictionary from the list to see the words. Go to 'Edit Dictionaries' to add new ones."
+        )
+        # get list of dictionaries from supabase
+        dict_reload = gr.Button("Reload dictionaries")
+        dictionary_list = gr.Dropdown(dict_list, label="Dictionaries")
+        dict_reload.click(get_dictionaries_titles, inputs=[], outputs=[dictionary_list])
+        dictionary_content = gr.Textbox(
+            "",
+            interactive=True,
+            max_lines=500,
+            lines=10,
+            label="Dictionary content",
+        )
+        btn_save_dictionary = gr.Button(value="Save")
+        btn_save_dictionary.click(
+            save_dictionary,
+            inputs=[dictionary_list, dictionary_content],
+            outputs=[dictionary_content],
+        )
+
+        dictionary_list.change(
+            show_dictionary,
+            inputs=[dictionary_list],
+            outputs=[dictionary_content],
+        )
+    with gr.Tab("Add Dictionaries"):
+        gr.Label(
+            "Choose dictionary from the list to see the words. Go to 'Edit Dictionaries' to add new ones."
+        )
+
+        dictionary_name = gr.Textbox(
+            "",
+            interactive=True,
+            max_lines=1,
+            lines=1,
+            label="New dictionary name",
+        )
+        dictionary_content = gr.Textbox(
+            "",
+            interactive=True,
+            max_lines=500,
+            lines=10,
+            label="Dictionary content",
+        )
+        btn_save_dictionary = gr.Button(value="Save")
+        btn_save_dictionary.click(
+            save_dictionary,
+            inputs=[dictionary_name, dictionary_content],
+            outputs=[dictionary_content],
+        )
+
+    with gr.Tab("About"):
+        gr.Label("Chat history analysis")
+        gr.Label("Todo: how to use this tool")
 
 
 demo.launch(server_name="0.0.0.0", server_port=8080)
